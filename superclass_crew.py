1d0a04a08020c6160cae8e14f0f943eb0c8e3a48
las2408 <las1@kent.ac.uk>
2026-03-16 19:57:16 +0000

    tidied the script up and changed a major error of property in the wrong place

--- superclass_crew.py
+++ superclass_crew.py
@@ -1,8 +1,7 @@
 #Superclass
 class CrewMember:
     def __init__(self, name, staff_id):
-        #Ensuring name entered isn't empty
-
+        #Ensuring name entered isn't empty so the system has a record
         if not name:
             raise ValueError("Name cannot be empty")
         
@@ -10,32 +9,38 @@ class CrewMember:
         self.staff_id = staff_id
 
     def __str__(self):
+        #Returns names and ID's in a clean format
         return f"Name: {self.name} | ID: {self.staff_id}"
 
-#Sub-Class
-class Pilot(CrewMember):
+#Sub-Class for Pilots
+class Pilot(CrewMember): #Using inheritance stops duplicate code
     def __init__(self, name, staff_id, flight_hours):
+        #Sends the common info back to the CrewMember class
         super().__init__(name, staff_id)
         self.flight_hours = flight_hours
 
+    #Protecting Data - Stops people from putting in impossible numbers breaking the code
+    @property
+    def flight_hours(self):
+        return self._flight_hours 
+
+    @flight_hours.setter
+    def flight_hours(self, value):
+        #This ensures people can't break the system via negative flight hours
+        if int(value) < 0:
+            raise ValueError("Flight hours cannot be negative!")
+        self._flight_hours = value
+
     def __str__(self):
+        #Assigns the staff member role and flight hours to the printout
         return super().__str__() + f" | Role: Pilot ({self.flight_hours} hours)"
 
-class Engineer(CrewMember):
+#Sub-Class for Engineers
+class Engineer(CrewMember): #Inherits from the main CrewMember class
     def __init__(self, name, staff_id, department):
         super().__init__(name, staff_id)
         self.department = department
 
     def __str__(self):
+        #Shows the specific department for the Engineer
         return super().__str__() + f" | Role: Engineer ({self.department})"
-
-#Protecting Data        
-@property
-def flight_hours(self):
-    return self._flight_hours
-
-@flight_hours.setter
-def flight_hours(self, value):
-    if int(value) < 0:
-            raise ValueError("Flight hours cannot be negative!")
-        self._flight_hours = value
